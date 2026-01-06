c6382646918dfccdba5fd80323f8ba1dc490dc93
Esmail7872 <mdesmailhosen13@gmail.com>
2026-01-07 01:56:00 +0600

    Finalize Firebase CDN configuration

--- Firebase.js
+++ Firebase.js
@@ -1,3 +1,5 @@
+// Firebase CDN config (Web only – no npm)
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -6,12 +8,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyAMXBGszT_7WaIfAy3SMcCfYG0ZKoY-_bA",
   authDomain: "rewardo-app-dacbb.firebaseapp.com",
   projectId: "rewardo-app-dacbb",
-  storageBucket: "rewardo-app-dacbb.appspot.com",
+  storageBucket: "rewardo-app-dacbb.firebasestorage.app",
   messagingSenderId: "936435804158",
   appId: "1:936435804158:web:1c48b5b2edad86f73102af"
 };
 
-const app = initializeApp(firebaseConfig);
-
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
