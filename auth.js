fef19cc5c5116546a4ceab1d8cd04082e323c6bf
Esmail7872 <mdesmailhosen13@gmail.com>
2026-01-07 00:54:32 +0600

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,41 @@
+import { auth, db } from "./firebase.js";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
+  doc, setDoc, getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+window.login = async () => {
+  await signInWithEmailAndPassword(
+    auth,
+    email.value,
+    password.value
+  );
+};
+
+window.register = async () => {
+  const res = await createUserWithEmailAndPassword(
+    auth,
+    email.value,
+    password.value
+  );
+
+  await setDoc(doc(db, "users", res.user.uid), {
+    coins: 0,
+    role: "user"
+  });
+};
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    document.getElementById("authBox").style.display = "none";
+    document.getElementById("userBox").style.display = "block";
+
+    const snap = await getDoc(doc(db, "users", user.uid));
+    document.getElementById("coin").innerText = snap.data().coins;
+  }
+});
