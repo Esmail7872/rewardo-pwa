786af2b3b9dc1121e1e00e739a48f2e0c7ccd734
Esmail7872 <mdesmailhosen13@gmail.com>
2026-01-07 13:06:15 +0600

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,8 +1,6 @@
-// Firebase CDN config (Web only – no npm)
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMXBGszT_7WaIfAy3SMcCfYG0ZKoY-_bA",
@@ -13,7 +11,7 @@ const firebaseConfig = {
   appId: "1:936435804158:web:1c48b5b2edad86f73102af"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
